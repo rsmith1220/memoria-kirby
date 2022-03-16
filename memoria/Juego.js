c06954c74451e6e062e9fa5e5dc1cebf73016fdf
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2022-03-16 17:41:03 -0600

    first upload

---
+++ memoria/Juego.js
@@ -0,0 +1,20 @@
+/*
+
+Rebecca Smith
+Laboratorio 6
+
+
+*/
+import React from 'React'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+ReactDOM.render(
+
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+
+)
